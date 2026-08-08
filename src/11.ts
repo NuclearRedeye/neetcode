7c7c690b6e31b5180d7308a65e75f5c6f4c747a6
NuclearRedeye <github@mneah.com>
2026-08-08 10:15:16 +0100

    feat: solution for challenge 11

---
+++ src/11.ts
@@ -0,0 +1,37 @@
+import { equals } from './utils/array.utils.ts';
+
+// function twoSum(numbers: number[], target: number): number[] {
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let ii = i + 1; ii < numbers.length; ii++) {
+//       if (numbers[i] + numbers[ii] === target) {
+//         return [1 + i, 1 + ii];
+//       }
+//     }
+//   }
+//   return [];
+// };
+
+function twoSum(numbers: number[], target: number): number[] {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [1 + left, 1 + right];
+    }
+    if (sum < target) {
+      left += 1;
+    }
+    if (sum > target) {
+      right -= 1;
+    }
+  }
+  return [];
+};
+
+console.assert(equals(twoSum([1, 2, 3, 4], 3), [1, 2]));
+console.assert(equals(twoSum([], 1), []));
+console.assert(equals(twoSum([1, 2, 3, 4], 7), [3, 4]));
+console.assert(equals(twoSum([2, 3], 5), [1, 2]));
+console.assert(equals(twoSum([-3, -1, 2, 4], 3), [2, 4]));
+console.assert(equals(twoSum([1, 2, 3, 4], 100), []));
