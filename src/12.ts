fe55e5082907c52560dca305b846effca90f3d41
NuclearRedeye <github@mneah.com>
2026-08-12 08:06:15 +0100

    refactor: improve challenge 12 solution

--- src/12.ts
+++ src/12.ts
@@ -1,35 +1,13 @@
 import { equals2d } from './utils/array.utils.ts';
 
-// function threeSum(numbers: number[]): number[][] {
-//   let retVal: number[][] = [];
-//   let seen = new Set<string>();
-//   for (let i = 0; i < numbers.length; i++) {
-//     for (let ii = 0; ii < numbers.length; ii++) {
-//       if (i === ii) continue;
-//       for (let iii = 0; iii < numbers.length; iii++) {
-//         if (iii === i || iii === ii) {
-//           continue;
-//         }
-
-//         if (numbers[i] + numbers[ii] + numbers[iii] === 0) {
-//           let indexes: number[] = [numbers[i], numbers[ii], numbers[iii]].sort((a,b) => a - b);
-//           if (seen.has(indexes.toString())) {
-//             continue;
-//           }
-//           seen.add(indexes.toString());
-//           retVal.push(indexes);
-//         }
-//       }
-//     }
-//   }
-//   return retVal;
-// }
-
 function threeSum(numbers: number[]): number[][] {
   let retVal: number[][] = [];
-  let seen = new Set<string>();
-  let sorted = numbers.toSorted((a, b) => a - b);
-  for (let i = 0; i < sorted.length; i++) {
+  const seen = new Set<string>();
+  const sorted = numbers.toSorted((a, b) => a - b);
+  for (let i = 0; i < sorted.length - 2; i++) {
+    if (sorted[i] > 0) {
+      break;
+    }
     let left = i + 1;
     let right = sorted.length - 1;
 
@@ -42,7 +20,7 @@ function threeSum(numbers: number[]): number[][] {
         right -= 1;
       }
       else {
-        let indexes: number[] = [sorted[i], sorted[left], sorted[right]].sort((a,b) => a - b);
+        const indexes: number[] = [sorted[i], sorted[left], sorted[right]];
         if (!seen.has(indexes.toString())) {
           seen.add(indexes.toString());
           retVal.push(indexes);
@@ -54,7 +32,6 @@ function threeSum(numbers: number[]): number[][] {
   }
   return retVal;
 }
-
 
 console.assert(equals2d(threeSum([]), []), 'empty');
 console.assert(equals2d(threeSum([0, 0]), []), 'too short');
