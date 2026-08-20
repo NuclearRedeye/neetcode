9ad8b8e0eb57d5a8d4f686c2da028491fa890a79
NuclearRedeye <github@mneah.com>
2026-08-20 20:13:17 +0100

    refactor: solution for challenge 15

--- src/15.ts
+++ src/15.ts
@@ -1,14 +1,14 @@
 function maxProfit(prices: number[]): number {
   let retVal = 0;
+  let lowest = 0;
   for (let i = 0; i < prices.length; i++) {
-    let highest = 0;
-    for (let ii = i; ii < prices.length; ii++) {
-      highest = Math.max(prices[ii] - prices[i], highest);
+    if (prices[lowest] > prices[i]) {
+      lowest = i;
     }
-    retVal = Math.max(retVal, highest);
+    retVal = Math.max(prices[i] - prices[lowest], retVal);
   }
   return retVal;
-};
+}
 
 console.assert(maxProfit([10, 1, 5, 6, 7, 1]) === 6, "example 1");
 console.assert(maxProfit([10, 8, 7, 5, 2]) === 0, "example 2");
